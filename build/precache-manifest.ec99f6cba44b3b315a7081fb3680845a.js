fbccd1cdde413882b64710d1ede204dda005fe67
aagjalpankaj <aagjalpankaj@gmail.com>
2020-06-21 08:54:20 +0530

    committing build folder

---
+++ build/precache-manifest.ec99f6cba44b3b315a7081fb3680845a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "526f4b67739d1ae67d4b50ce17194f84",
+    "url": "/find-my-events/index.html"
+  },
+  {
+    "revision": "89be373479d78bdb2623",
+    "url": "/find-my-events/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "0aa681d8fa121d466950",
+    "url": "/find-my-events/static/css/main.dd944a0e.chunk.css"
+  },
+  {
+    "revision": "89be373479d78bdb2623",
+    "url": "/find-my-events/static/js/2.6e7cadf7.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/find-my-events/static/js/2.6e7cadf7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0aa681d8fa121d466950",
+    "url": "/find-my-events/static/js/main.8e1e60bc.chunk.js"
+  },
+  {
+    "revision": "e74e010a80d51841c6de",
+    "url": "/find-my-events/static/js/runtime-main.f4aa39f4.js"
+  }
+]);
